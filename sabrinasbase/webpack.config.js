0b9d5d7c4c8ea09f6d410077773141f56805f573
sabrinaspage <sabreyes01@gmail.com>
2021-01-21 01:31:59 -0500

    Revert "Merge branch 'main' of https://github.com/sabrinaspage/sabrinasbase into main"
    This reverts commit 6d0286600a9af52ff3ddd9a61527880cdcd34afa, reversing
    changes made to cfac82038accfafb78e0da631998a8627479d577.

--- sabrinasbase/webpack.config.js
+++ sabrinasbase/webpack.config.js
@@ -1,45 +1,28 @@
-var path = require("path");
-
 module.exports = {
-  //mode: "production",
-  mode: "development",
-  devtool: "inline-source-map",
-  context: path.join(__dirname, "pages"),
-
-  entry: ["/_app.js" /*main*/],
-  output: {
-    filename: "./bundle.js", // in /dist
-  },
-  resolve: {
-    // Add `.ts` and `.tsx` as a resolvable extension.
-    extensions: [".ts", ".tsx", ".js", ".css", ".scss"],
-  },
-  module: {
-    rules: [
-      { test: /\.tsx?$/, loader: "ts-loader" },
-
-      {
-        test: /\.scss$/,
-        use: [
-          { loader: "style-loader" }, // to inject the result into the DOM as a style block
-          { loader: "css-modules-typescript-loader" }, // to generate a .d.ts module next to the .scss file (also requires a declaration.d.ts with "declare modules '*.scss';" in it to tell TypeScript that "import styles from './styles.scss';" means to load the module "./styles.scss.d.td")
-          { loader: "css-loader", options: { modules: true } }, // to convert the resulting CSS to Javascript to be bundled (modules:true to rename CSS classes in output to cryptic identifiers, except if wrapped in a :global(...) pseudo class)
-          { loader: "sass-loader" }, // to convert SASS to CSS
-          // NOTE: The first build after adding/removing/renaming CSS classes fails, since the newly generated .d.ts typescript module is picked up only later
-        ],
+    //mode: "production", 
+      mode: "development", devtool: "inline-source-map",
+  
+      entry: [ "./src/app.tsx"/*main*/ ], 
+      output: {
+          filename: "./bundle.js"  // in /dist
       },
-      {
-        test: /\.png|jpg|gif$/i,
-        use: [
-          {
-            loader: "url-loader",
-            options: {
-              limit: 8192,
-            },
-          },
-        ],
+      resolve: {
+          // Add `.ts` and `.tsx` as a resolvable extension.
+          extensions: [".ts", ".tsx", ".js", ".css", ".scss"]
       },
-      { test: /\.js|tsx|ts$/, loader: "babel-loader", exclude: /node_modules/ },
-    ],
-  },
-};
+      module: {
+          rules: [
+  
+              { test: /\.tsx?$/, loader: "ts-loader" }, 
+  
+              { test: /\.scss$/, use: [ 
+                  { loader: "style-loader" },  // to inject the result into the DOM as a style block
+                  { loader: "css-modules-typescript-loader"},  // to generate a .d.ts module next to the .scss file (also requires a declaration.d.ts with "declare modules '*.scss';" in it to tell TypeScript that "import styles from './styles.scss';" means to load the module "./styles.scss.d.td")
+                  { loader: "css-loader", options: { modules: true } },  // to convert the resulting CSS to Javascript to be bundled (modules:true to rename CSS classes in output to cryptic identifiers, except if wrapped in a :global(...) pseudo class)
+                  { loader: "sass-loader" },  // to convert SASS to CSS
+                  // NOTE: The first build after adding/removing/renaming CSS classes fails, since the newly generated .d.ts typescript module is picked up only later
+              ] }, 
+  
+          ]
+      }
+  }; 
