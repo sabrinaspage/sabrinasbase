613c6579652b90d36c8244e4e52e277978c176ad
sabrinaspage <sabreyes01@gmail.com>
2021-01-21 05:10:25 -0500

    navigation

--- sabrinasbase/components/Navigation/NavigationBar.tsx
+++ sabrinasbase/components/Navigation/NavigationBar.tsx
@@ -5,11 +5,17 @@ import styles from "../../styles/components/NavigationBar.module.scss";
 
 interface Props {
   data: NavCard[];
+  marginTop?: number;
+  marginBottom?: number;
+  marginLeft?: number;
+  marginRight?: number;
 }
 
-const NavigationBar: React.FC<Props> = ({ data = [] }) => {
+const NavigationBar: React.FC<Props> = ({ data = [], marginTop = 0, marginBottom, marginLeft, marginRight }) => {
   return (
-    <div className={styles.container}>
+    <div className={styles.container} style={{
+      margin: marginTop, marginRight, marginBottom, marginLeft
+    }}>
       {data.map((i, key) => {
         return <NavigationCard key={key} name={i.name} page={i.page} />;
       })}
